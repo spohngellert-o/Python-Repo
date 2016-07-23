0ac30ca7f6ea97de2d69141ff4c7623022193b2d
John Doe <spohngellert.o@husky.neu.edu>
2016-07-23 10:28:40 -0400

    Successfully implemented user login.

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.contrib.auth import authenticate
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 
 # ...
 def getHomePage(request):
@@ -14,16 +15,11 @@ def getHomePage(request):
 def login(request):
 	c = {}
 	c.update(csrf(request))
-	try:
-		user = authenticate(username=request.POST['username'], password=request.POST['password'])
-		if user is not None:
-			# the password verified for the user
-			if user.is_active:
-				return render_to_response("Valid", c)
-			else:
-				return render_to_response("Inactive", c)
-		else:
-			# the authentication system was unable to verify the username and password
-			return render_to_response("Invalid", c)
-	except (KeyError):
-		return render_to_response("Invalid", c)
+	user = authenticate(username=request.POST.get('username', ''), password=request.POST.get('password', ''))
+	if user is not None:
+		# the password verified for the user
+		auth.login(request, user)
+		return HttpResponseRedirect("/polls")
+	else:
+		# the authentication system was unable to verify the username and password
+		return render_to_response("home/failure.html", c)
